69622a7c884d4b64f83a056c49d245e590c6edea
jeon9825 <jeon9825@naver.com>
2019-10-08 09:50:17 +0900

    191008 InflationTest

--- InflationTest/app/src/main/java/cse/mobile/inflationtest/MainActivity.java
+++ InflationTest/app/src/main/java/cse/mobile/inflationtest/MainActivity.java
@@ -2,7 +2,10 @@ package cse.mobile.inflationtest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -18,15 +21,27 @@ public class MainActivity extends AppCompatActivity {
         TextView tvHello = new TextView(this);
         tvHello.setText("Hello, Android in Code");
 
-        Button btOK = new Button(this);
-        btOK.setText("OK in Code.");
+//        Button btOK = new Button(this);
+//        btOK.setText("OK in Code.");
+
+//      위와 같은 코드임. 세가지 방법이 있음.
+//        LayoutInflater inflater = getLayoutInflater();
+//        LayoutInflater inflater1 = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+//        LayoutInflater inflater2 = LayoutInflater.from(this);
+//
+//        Button btMyButton = (Button) inflater.inflate(R.layout.mybutton, null);
+
+        //위와 같은 코드임. 가장 많이 쓰는 방법
+        Button btMyButton = (Button) View.inflate(this, R.layout.mybutton, null);
 
         LinearLayout linearLayout = new LinearLayout(this);
         linearLayout.setOrientation(LinearLayout.VERTICAL);
 
-        LinearLayout.LayoutParams llParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        linearLayout.addView(tvHello,llParams);
-        linearLayout.addView(btOK,llParams);
+        LinearLayout.LayoutParams llParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        linearLayout.addView(tvHello, llParams);
+//        linearLayout.addView(btOK,llParams);
+//        linearLayout.addView(btMyButton); //addView의 default가 width=MATCH_PARENT, height=WRAP_CONTENT
+        linearLayout.addView(btMyButton,llParams); 
 
         setContentView(linearLayout);
     }
